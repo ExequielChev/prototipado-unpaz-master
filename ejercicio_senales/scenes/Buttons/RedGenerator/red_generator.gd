02c5aa98bd6cabd069f1ef6687e5126c1ca79cf6
Zahir Cheveste <zahir.cheveste@hotmail.com>
2025-04-30 09:15:38 -0300

    Funcion generate_block

--- ejercicio_senales/scenes/Buttons/RedGenerator/red_generator.gd
+++ ejercicio_senales/scenes/Buttons/RedGenerator/red_generator.gd
@@ -1,1 +1,10 @@
 extends TextureButton
+
+
+func _on_red_generator_button_down():
+	generate_block()
+	
+func generate_block():
+	var block = preload("res://scenes/Blocks/Red/BlockRed.tscn").instantiate()
+	block.position = position + Vector2(0, 50 * get_child_count())
+	get_parent().add_child(block)
--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -11,11 +11,7 @@ func _ready():
 	pass
 
 
-func _on_red_generator_button_down() -> void:
-	pass # Replace with function body.
 
 
-func _on_violet_generator_button_down() -> void:
-	pass # Replace with function body.
 	
 	
