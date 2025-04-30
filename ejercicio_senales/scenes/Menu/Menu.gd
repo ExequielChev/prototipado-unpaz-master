78ff2692d3d72553fa1e342a37280ac46b418ea3
Zahir Cheveste <zahir.cheveste@hotmail.com>
2025-04-30 09:56:17 -0300

    funcion count_new_instance

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -4,6 +4,10 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
 
+func count_new_instance():
+	instancesCount += 1
+	$CountLabel.text = "Cantidad de instancias: %s" % instancesCount
+
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
